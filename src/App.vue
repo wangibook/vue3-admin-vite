f789a6b432915cc127c509d5f662232fe4ba4ee6
wangyong <wangyong@bdxdoctor.com>
2023-07-11 15:34:33 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { h } from "vue"
+import { useTheme } from "@/hooks/useTheme"
+import { resetConfigLayout } from "@/utils"
+// 将 Element Plus 的语言设置为中文
+import zhCn from "element-plus/lib/locale/lang/zh-cn"
+
+const { initTheme } = useTheme()
+
+/** 初始化主题 */
+initTheme()
+
+</script>
+
+<template>
+  <ElConfigProvider :locale="zhCn">
+    <router-view />
+  </ElConfigProvider>
+</template>
